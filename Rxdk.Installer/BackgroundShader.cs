e445337dec3c45711642ae5422b9594db7b92299
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-24 02:50:31 -0400

    Add installer

---
+++ Rxdk.Installer/BackgroundShader.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace Rxdk.Installer
+{
+    internal class BackgroundShader : ShaderEffect
+    {
+        private static readonly PixelShader pixelShader = new PixelShader
+        {
+            UriSource = new Uri("/Resources/BackgroundShader.ps", UriKind.Relative)
+        };
+
+        public BackgroundShader()
+        {
+            PixelShader = pixelShader;
+            UpdateShaderValue(InputProperty);
+            UpdateShaderValue(TimeProperty);
+        }
+
+        public Brush Input
+        {
+            get => (Brush)GetValue(InputProperty);
+            set => SetValue(InputProperty, value);
+        }
+
+        public static readonly DependencyProperty InputProperty =
+            ShaderEffect.RegisterPixelShaderSamplerProperty(
+                "Input",
+                typeof(BackgroundShader),
+                0);
+
+        public double Time
+        {
+            get => (double)GetValue(TimeProperty);
+            set => SetValue(TimeProperty, value);
+        }
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register(
+                "Time",
+                typeof(double),
+                typeof(BackgroundShader),
+                new UIPropertyMetadata(
+                    0.0,
+                    PixelShaderConstantCallback(0)));
+    }
+}
